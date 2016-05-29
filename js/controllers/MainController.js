d651039baa49287ce6309e6ee6cbce11e028d423
anansharm <anansharm@gmail.com>
2016-05-28 20:37:48 -0700

    adding some more controller

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,3 +1,11 @@
 app.controller('MainController', ['$scope', function($scope){
 	$scope.title = 'This is great';
+	$scope.promo = 'A promotion to give you a good discount';
+	$scope.product = {
+		name: 'Moving',
+		price: 115
+	};
+}]);
+app.controller("ChildController", ["$scope",function($scope){
+	$scope.title = "What is going on!";
 }]);
