56824299f20e21bf7f45b1c0dfaed0c08b2a26d1
anansharm <anansharm@gmail.com>
2016-06-04 16:18:07 -0700

    adding some bootstrap classes and adding object array in controller to display in view

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,11 +1,20 @@
 app.controller('MainController', ['$scope', function($scope){
 	$scope.title = 'This is great';
 	$scope.promo = 'A promotion to give you a good discount';
-	$scope.product = {
-		name: 'Moving',
-		price: 115,
-		pubdate: new Date('1980', '00', '26')
-	};
+	$scope.products = [{
+						name: 'Moving',
+						price: 115,
+						pubdate: new Date('1980', '00', '26'),
+						cover: 'img/book-your-move.jpg'
+					},
+					{
+						name: 'Cleaning',
+						price: 110,
+						pubdate: new Date('1980', '00', '26'),
+						comlogo: 'img/moving-company.jpg'
+					}
+
+					];
 }]);
 app.controller("ChildController", ["$scope",function($scope){
 	$scope.title = "What is going on!";
